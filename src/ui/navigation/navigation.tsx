285b6f94eae121513309d5d3e16d9c060768e8d8
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-24 10:00:13 -0400

    partial; add toggle functionality without design

--- src/ui/navigation/navigation.tsx
+++ src/ui/navigation/navigation.tsx
@@ -5,6 +5,7 @@ import hamburger from "../../../public/shared/mobile/icon-hamburger.svg";
 import close from "../../../public/shared/mobile/icon-close.svg";
 import Image from "next/image";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Navigation(){
 
@@ -19,6 +20,17 @@ export default function Navigation(){
             <button className={`${style["nav__toggle"]}`} onClick={toggle}>
                 <Image src={isOpen ? close : hamburger} alt={isOpen ? "close button" : "hamburger button"}/>
             </button>
+            <ul className={`${style["nav__links"]} ${isOpen ? style["nav__links--show"] : ""}`}>
+                <li className={`${style["nav__item"]}`}>
+                    <Link href={"/"} className={`${style["nav__link"]}`}>OUR COMPANY</Link>
+                </li>
+                <li>
+                    <Link href={"/"} className={`${style["nav__link"]}`}>ABOUT</Link>
+                </li>
+                <li>
+                    <Link href={"/"} className={`${style["nav__link"]}`}>CONTACT</Link>
+                </li>
+            </ul>
         </div>
     );
 }
