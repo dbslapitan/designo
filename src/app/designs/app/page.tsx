e1e7ee4b2597f6066ee3726bf0455b6a05cbb975
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-25 21:11:01 -0400

    finish design of all design pages on all queries

--- src/app/designs/app/page.tsx
+++ src/app/designs/app/page.tsx
@@ -1,6 +1,13 @@
 import CallToAction from "@/ui/call-to-action/call-to-action";
 import styles from "./page.module.scss";
 import TitleSection from "@/ui/title-section/title-section";
+import DesignLinks from "@/ui/design-links/design-links";
+import airFilter from "../../../../public/app-design/desktop/image-airfilter.jpg";
+import eyecam from "../../../../public/app-design/desktop/image-eyecam.jpg";
+import faceit from "../../../../public/app-design/desktop/image-faceit.jpg";
+import loopStudios from "../../../../public/app-design/desktop/image-loopStudios.jpg";
+import todo from "../../../../public/app-design/desktop/image-todo.jpg";
+import Card from "@/ui/card/card";
 
 export default function AppDesigns(){
 
@@ -8,9 +15,43 @@ export default function AppDesigns(){
         title: "App Design", 
         description: "Our mobile designs bring intuitive digital solutions to your customers right at their fingertips."
     }
+    
+    const content = [{
+            image: airFilter,
+            title: "AIRFILTER",
+            description: "Solving the problem of poor indoor air quality by filtering the air"
+        }, {
+            image: eyecam,
+            title: "EYECAM",
+            description: "Product that lets you edit your favorite photos and videos at any time"
+        }, {
+            image: faceit,
+            title: "FACEIT",
+            description: "Get to meet your favorite internet superstar with the faceit app"
+        }, {
+            image: todo,
+            title: "TODO",
+            description: "A todo app that features cloud sync with light and dark mode"
+        }, {
+            image: loopStudios,
+            title: "LOOPSTUDIOS",
+            description: "A VR experience app made for Loopstudios"
+        }
+    ];
+
     return(
         <main className={`${styles["web"]}`}>
             <TitleSection data={data}/>
+            <ul className={`${styles["web__cards"]}`}>
+                {
+                    content.map(content => {
+                        return(
+                            <Card key={content.title} data={content}/>
+                        );
+                    })
+                }
+            </ul>
+            <DesignLinks current="app" />
             <CallToAction />
         </main>
     );
--- src/app/designs/graphic/page.tsx
+++ src/app/designs/graphic/page.tsx
@@ -1,6 +1,12 @@
 import CallToAction from "@/ui/call-to-action/call-to-action";
 import styles from "./page.module.scss";
 import TitleSection from "@/ui/title-section/title-section";
+import DesignLinks from "@/ui/design-links/design-links";
+import science from "../../../../public/graphic-design/desktop/image-science.jpg";
+import boxed from "../../../../public/graphic-design/desktop/image-boxed-water.jpg";
+import change from "../../../../public/graphic-design/desktop/image-change.jpg";
+import Card from "@/ui/card/card";
+
 
 export default function GraphicDesigns(){
 
@@ -8,9 +14,35 @@ export default function GraphicDesigns(){
         title: "Graphic Design", 
         description: "We deliver eye-catching branding materials that are tailored to meet your business objectives."
     }
+
+    const content = [{
+        image: change,
+        title: "TIM BROWN",
+        description: "A book cover designed for Tim Brown’s new release, ‘Change’"
+        }, {
+            image: boxed,
+            title: "BOXED WATER",
+            description: "A simple packaging concept made for Boxed Water"
+        }, {
+            image: science,
+            title: "SCIENCE!",
+            description: "A poster made in collaboration with the Federal Art Project"
+        }
+    ];
+
     return(
         <main className={`${styles["web"]}`}>
             <TitleSection data={data}/>
+            <ul className={`${styles["web__cards"]}`}>
+                {
+                    content.map(content => {
+                        return(
+                            <Card key={content.title} data={content}/>
+                        );
+                    })
+                }
+            </ul>
+            <DesignLinks current="graphic" />
             <CallToAction />
         </main>
     );
