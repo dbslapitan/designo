00a75ae9e78a29496300534da7e9b7e1246c5071
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-26 05:30:20 -0400

    finish design on tablet

--- src/ui/single-card/single-card.tsx
+++ src/ui/single-card/single-card.tsx
@@ -1,17 +1,16 @@
 import { StaticImageData } from "next/image";
 import styles from "./single-card.module.scss";
 
-export default function SingleCard({className = "", data, hero = false}: {className?: string, data: {image: StaticImageData, title: string, paragrahps: string[]}, hero?: boolean}){
+export default function SingleCard({className = "", data, hero = false, cover}: {cover: string, className?: string, data: {image: StaticImageData, title: string, paragrahps: string[]}, hero?: boolean}){
 
 
     return(
         <section className={`${styles["card"]} ${className ? className : ""}`}>
-            <div className={`${styles["card__first"]}`}>
-                <img src={data.image.src} alt={`cover picture for ${data.title}`}  className={`${styles["card__cover"]}`}/>
+            <div className={`${styles["card__first"]} ${styles[`card__first--${cover}`]}`}>
             </div>
             <div className={`${styles["card__last"]} ${hero ? styles["card__last--peach"] : styles["card__last--light"]}`}>
                 {hero && <h1 className={`${styles["card__title"]}`}>{data.title}</h1>}
-                {!hero && <h2 className={`${styles["card__title"]}`}>{data.title}</h2>}
+                {!hero && <h2 className={`${styles["card__title"]} ${styles["card__title--h2"]}`}>{data.title}</h2>}
                 {
                     data.paragrahps.map(paragraph => {
                         return(
