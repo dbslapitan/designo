75b511771a4cd0317da3e71410c9975215ef5c3c
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-27 00:20:02 -0400

    remove call to action on contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.scss";
+
+export default function Contact(){
+
+    return(
+        <main>
+            <h1>COntact Page</h1>
+        </main>
+    );
+}
--- src/ui/call-to-action/call-to-action.tsx
+++ src/ui/call-to-action/call-to-action.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./call-to-action.module.scss";
+import { usePathname } from "next/navigation";
 
 export default function CallToAction() {
+
+    const path = usePathname();
+    const isContanct = path.toLowerCase().includes("/contact");
+
     return (
-        <div className={`${styles["container"]}`}>
+        <div className={`${styles["container"]} ${isContanct ? styles["container--hide"] : ""}`}>
             <section className={`${styles["action"]}`}>
                 <h2 className={`${styles["action__header"]}`}>Let&apos;s talk about your project</h2>
                 <p className={`${styles["action__paragraph"]}`}>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
