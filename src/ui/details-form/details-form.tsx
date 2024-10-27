5d67251730386ecb44c0c5d8163ecc9a8c7c407c
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-27 06:30:01 -0400

    add error handling

--- src/ui/details-form/details-form.tsx
+++ src/ui/details-form/details-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FocusEvent, FormEvent, MutableRefObject, useRef, useState } from "react";
+import { ChangeEvent, FocusEvent, FormEvent, MutableRefObject, useRef, useState } from "react";
 import styles from "./details-form.module.scss";
 import isEmail from "validator/lib/isEmail";
 import isMobile from "validator/lib/isMobilePhone";
@@ -43,58 +43,60 @@ export default function DetailsFrom(){
         const target = (event.target as HTMLInputElement);
         const value = `${target.value.trim()}`;
         const id = target.id;
-        const temp = {...details};
-        const tempErrors = {...errors};
         if(!value){
             if((ref.current as HTMLLabelElement).classList.contains("field__label--hide")){
                 (ref.current as HTMLLabelElement).classList.remove("field__label--hide");
             }
             (event.target as HTMLInputElement).value = "";
         }
-        temp[id as keyof typeof details] = value;
-        if(!value){
-            tempErrors[id as keyof typeof errors] = "Can't be Empty";
-        }
-        else{
-            tempErrors[id as keyof typeof errors] = "";
-        }
-        if(id === "email"){
-            tempErrors.email = isEmail(value) ? "" : "Email is not valid";
-        }
-        else if(id === "phone"){
-            tempErrors.phone = isMobile(value) ? "" : "Phone is not valid";
-        }
-        setDetails(temp);
-        setErrors(tempErrors);
     }
 
     const submitHandler = (event: FormEvent) => {
         event.preventDefault();
-        setIsSubmit(true);
-        console.log(details);
-        console.log(errors);
+        const {name, phone, email, message} = details;
+
+        setErrors({
+            name: name ? "" : "Can't be empty",
+            email: isEmail(email) ? "" : "Invalid format",
+            phone: isMobile(phone) ? "" : "Invalid format",
+            message: message ? "" : "Can't be empty"
+        });
+
+        if(!name || !isEmail(email) || !isMobile(phone) || !message){
+            setIsSubmit(true);
+        }
+    }
+
+    const changeHandler = (event: ChangeEvent) => {
+        const target = (event.target as HTMLInputElement);
+        const id = target.id;
+        const temp = {...details};
+
+        temp[id as keyof typeof details] = target.value;
+        setDetails(temp);
     }
 
     return(
         <form className={`${styles["detail"]}`} onSubmit={submitHandler}>
             <div className={`field ${styles["detail__field"]}`}>
                 <label htmlFor="name" className={`field__label`} ref={nameRef}>Name</label>
-                <input type="text" name="name" id="name" className={`field__input`} onFocus={(e) => focusHandler(e, nameRef)} onBlur={(e) => focusOutHandler(e, nameRef)}/>
+                <input defaultValue={details.name} type="text" name="name" id="name" className={`field__input`} onFocus={(e) => focusHandler(e, nameRef)} onBlur={(e) => focusOutHandler(e, nameRef)} onChange={changeHandler}/>
                 {(isSubmit && !!errors.name) ? <p className={`field__error`}>{errors.name}</p> : null}
             </div>
             <div className={`field ${styles["detail__field"]}`}>
                 <label htmlFor="email" className={`field__label`} ref={emailRef}>Email</label>
-                <input type="text" name="email" id="email" className={`field__input`} onFocus={(e) => focusHandler(e, emailRef)} onBlur={(e) => focusOutHandler(e, emailRef)}/>
+                <input defaultValue={details.email} type="text" name="email" id="email" className={`field__input`} onFocus={(e) => focusHandler(e, emailRef)} onBlur={(e) => focusOutHandler(e, emailRef)} onChange={changeHandler}/>
                 {(isSubmit && !!errors.email) ? <p className={`field__error`}>{errors.email}</p> : null}
             </div>
             <div className={`field ${styles["detail__field"]}`}>
                 <label htmlFor="phone" className={`field__label`} ref={phoneRef}>Phone</label>
-                <input type="text" name="phone" id="phone" className={`field__input`} onFocus={(e) => focusHandler(e, phoneRef)} onBlur={(e) => focusOutHandler(e, phoneRef)}/>
+                <input defaultValue={details.phone} type="text" name="phone" id="phone" className={`field__input`} onFocus={(e) => focusHandler(e, phoneRef)} onBlur={(e) => focusOutHandler(e, phoneRef)} onChange={changeHandler}/>
                 {(isSubmit && !!errors.phone) ? <p className={`field__error`}>{errors.phone}</p> : null}
             </div>
             <div className={`field ${styles["detail__field"]}`}>
                 <label htmlFor="message" className={`field__label field__label--textarea`} ref={messageRef}>Your Message</label>
-                <textarea name="message" id="message" className={`field__input field__input--textarea`} onFocus={(e) => focusHandler(e, messageRef)} onBlur={(e) => focusOutHandler(e, messageRef)}></textarea>
+                <textarea defaultValue={details.message} name="message" id="message" className={`field__input field__input--textarea`} onFocus={(e) => focusHandler(e, messageRef)} onBlur={(e) => focusOutHandler(e, messageRef)} onChange={changeHandler}>
+                </textarea>
                 {(isSubmit && !!errors.message) ? <p className={`field__error`}>{errors.message}</p> : null}
             </div>
             <button className={`btn btn__dark ${styles["detail__submit"]}`}>SUBMIT</button>
