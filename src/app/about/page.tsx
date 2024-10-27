020bdf26b7ad1bc07d21f5ef2a87aec05199688f
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-27 15:33:50 -0400

    change css issue of single card module

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,8 +6,6 @@ import SingleCard from "@/ui/single-card/single-card";
 import canada from "../../../public/shared/desktop/illustration-canada.svg";
 import australia from "../../../public/shared/desktop/illustration-australia.svg";
 import uk from "../../../public/shared/desktop/illustration-united-kingdom.svg";
-import Image from "next/image";
-import Link from "next/link";
 import Countries from "@/ui/countries/countries";
 
 export default function About() {
@@ -28,7 +26,7 @@ export default function About() {
             <SingleCard cover="hero" hero={true} data={{ image: hero, title: "About Us", paragrahps: ["Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences."] }} />
             <SingleCard cover="world" data={{ image: world, title: "World-class talent", paragrahps: ["We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.", "Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission."] }}/>
             <Countries />
-            <SingleCard cover="real" data={{ image: real, title: "The real deal", paragrahps: ["As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.", "We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results."] }} />
+            <SingleCard cover="real" className={`${styles["card--top"]}`} data={{ image: real, title: "The real deal", paragrahps: ["As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.", "We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results."] }} />
         </main>
     );
 }
