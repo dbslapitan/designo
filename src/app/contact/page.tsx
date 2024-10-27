ccec3cb2ec7cade9fe7082dfdf3cc33ba15d1d30
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-27 16:40:08 -0400

    finish contant page on all media

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,8 +7,10 @@ export default function Contact(){
     return(
         <main className={`${styles["contact"]}`}>
             <section className={`${styles["header"]}`}>
-                <h1 className={`${styles["header__title"]}`}>Contact Us</h1>
-                <p className={`${styles["header__description"]}`}>{`Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.`}</p>
+                <div  className={`${styles["header__top"]}`}>
+                    <h1 className={`${styles["header__title"]}`}>Contact Us</h1>
+                    <p className={`${styles["header__description"]}`}>{`Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.`}</p>
+                </div>
                 <DetailsFrom />
             </section>
             <Countries />
